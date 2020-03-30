d8ea53e0643f6f07129b640b18d0e96b02fdf94f
targhan@gmail.com <Dyganjp0>
2020-03-30 17:15:15 +0800

    added more opencv functions and resources

--- PyFlow/Packages/PyFlowOpenCv/Pins/ImagePin.py
+++ PyFlow/Packages/PyFlowOpenCv/Pins/ImagePin.py
@@ -2,14 +2,17 @@ from PyFlow.Core import PinBase
 from PyFlow.Core.Common import *
 import numpy as np
 import json
+
+
 class MyImage():
-    def __init__(self,image=None):
-        if isinstance(image,MyImage):
+    def __init__(self, image=None):
+        if isinstance(image, MyImage):
             self.image = image.image.copy()
-        elif isinstance(image,np.ndarray):
+        elif isinstance(image, np.ndarray):
             self.image = image
         else:
             self.image = np.zeros((2, 2, 3), np.uint8)
+
 
 class NoneEncoder(json.JSONEncoder):
     def default(self, vec3):
@@ -23,8 +26,10 @@ class NoneDecoder(json.JSONDecoder):
     def object_hook(self, vec3Dict):
         return MyImage()
 
+
 class ImagePin(PinBase):
     """doc string for ImagePin"""
+
     def __init__(self, name, parent, direction, **kwargs):
         super(ImagePin, self).__init__(name, parent, direction, **kwargs)
         self.setDefaultValue(MyImage())
